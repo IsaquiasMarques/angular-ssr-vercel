a6286b0a967260e932f1d1e301dbe0230d9937f5
IsaquiasMarques <patisaquias2000@gmail.com>
2024-02-25 10:51:02 +0100

    api/index.js changed for testing

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-const server = import('../dist/angular-vercel/server/main.server.mjs');
+// const server = import('../dist/angular-vercel/server/main.server.mjs');
 
-module.exports = server.app;
+// module.exports = server.app;
+
+import * as server from '../dist/angular-vercel/server/server.mjs';
+
+export default server.app();
