b3116a78d141bd9bff0187fde2a0e190d1409600
IsaquiasMarques <patisaquias2000@gmail.com>
2024-02-25 12:11:05 +0100

    root route recovered

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
   { path: 'post/:slug', loadChildren: () => import('./post/post.module').then(m => m.PostModule) },
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
 ];
 
 @NgModule({
